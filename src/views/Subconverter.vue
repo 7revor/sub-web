13030ceb9609c191f3889fa6fd1c28696e42486d
7revor <7revor233@gmail.com>
2023-05-26 11:48:23 +0800

    feat: 添加配置文件

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -239,6 +239,10 @@ export default {
                 value: "https://raw.githubusercontent.com/7revor/proxy-rules/main/subconverter.min.ini",
               },
               {
+                label: "7revor（自用精简多机场备选）",
+                value: "https://raw.githubusercontent.com/7revor/proxy-rules/main/subconverter.min.ini",
+              },
+              {
                 label: "7revor（自用全分组）",
                 value: "https://raw.githubusercontent.com/7revor/proxy-rules/main/subconverter.config.ini",
               },
